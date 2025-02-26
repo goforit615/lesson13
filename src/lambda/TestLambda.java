9a3fbca64631cd0ad57515da523b9bbe43e8e8b2
goforit615 <bupark82@gsm.hs.kr>
2025-02-26 09:09:52 +0900

    return value TestLambda

--- src/lambda/TestLambda.java
+++ src/lambda/TestLambda.java
@@ -9,8 +9,14 @@ public class TestLambda {
         PrintString lambdaStr = s -> System.out.println(s);
         lambdaStr.showString("hello lambda_1");
         showMyString(lambdaStr);
+        PrintString reStr = returnString(); // 변수로 반환하기
+        reStr.showString("hello");  // 메서드 호출
     }
     public static void showMyString(PrintString p){
         p.showString("hello lambda_2");
+    }
+
+    public static PrintString returnString(){
+        return s -> System.out.println(s + "world");
     }
 }
